ead448fe2e0d8cf509e18f716b38784e7a470a86
Aushah Gowhar <waniaushahgowhar@gmail.com>
2026-02-10 22:17:51 +0530

    Made basic structure ,made register and login functionality

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,106 @@
+const userModel = require("../models/user.model");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+
+async function loginController(req, res) {
+  const { email, username, password } = req.body;
+
+  const user = await userModel.findOne({
+    $or: [
+      {
+        username: username,
+      },
+      {
+        email: email,
+      },
+    ],
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+
+  const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+  const isPasswordValid = hash == user.password;
+
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: "Incorrect password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+
+  res.cookie("token", token);
+
+  res.status(200).json({
+    message: "User LoggedIn Successfully",
+    user: {
+      email: user.email,
+      username: user.username,
+      bio: user.bio,
+      profileImage: user.profileImage,
+    },
+  });
+}
+
+async function registerController(req, res) {
+  const { email, username, password, bio, profileImage } = req.body;
+
+  const isUserAlreadyExists = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (isUserAlreadyExists) {
+    return res.status(409).json({
+      message:
+        isUserAlreadyExists.email == email
+          ? "Email already exist"
+          : "Username already exist",
+    });
+  }
+
+  const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+  const user = await userModel.create({
+    username,
+    email,
+    bio,
+    profileImage,
+    password: hash,
+  });
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User registered Successfully",
+    user: {
+      email: user.email,
+      username: user.username,
+      bio: user.bio,
+      profileImage: user.profileImage,
+    },
+  });
+}
+
+module.exports = {
+  registerController,
+  loginController,
+};
