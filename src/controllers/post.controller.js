257cfcfb2123fa825d674a921eaa785f40e28c91
Aushah Gowhar <waniaushahgowhar@gmail.com>
2026-02-13 08:16:34 +0530

    Post creation

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,20 +1,34 @@
 const postModel = require("../models/post.model");
 const ImageKit = require("@imagekit/nodejs");
 const { toFile } = require("@imagekit/nodejs");
+const jwt = require("jsonwebtoken");
 
 const imagekit = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
 
 async function createPostController(req, res) {
-  console.log(req.body, req.file);
+  const token = req.cookies.token;
+
+  let user = null;
+  try {
+    user = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    res.status(401).json("Unauthorized Acess");
+  }
 
   const file = await imagekit.files.upload({
     file: await toFile(Buffer.from(req.file.buffer), "file"),
     fileName: "Test",
+    folder: "insta-clone/",
   });
 
-  res.send(file);
+  let post = await postModel.create({
+    caption: req.body.caption,
+    imageUrl: file.url,
+    user: user.id,
+  });
+  res.send(post);
 }
 
 module.exports = createPostController;
